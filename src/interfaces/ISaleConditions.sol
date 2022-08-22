0030cfde928f227c8745d1548b2642c3a28d4eb6
Théo RICHARD <theo.richard@omniadefi.com>
2022-08-22 12:22:42 +0100

    feat(interface): update sale conditions

--- src/interfaces/ISaleConditions.sol
+++ src/interfaces/ISaleConditions.sol
@@ -1,14 +1,7 @@
 // SPDX-License-Identifier: SPDX-2.0-or-later
 pragma solidity ^0.8.13;
 
-/**
- * @notice Set the conditions for a sale of a specific asset as soon as it get listed.
- *
- *         The owner can update the conditions at any point in time prior an offer
- *         approval.
- *         This means it is up to the buyer to find an agreement (off-chain) with the
- *         seller.
- */
+import {AssetNft} from "omnia-nft/AssetNft.sol";
 
 interface ISaleConditions {
     struct PaymentTerms {
@@ -16,40 +9,40 @@ interface ISaleConditions {
         uint256 depositReceiptTimeframe; // at least 8 hours
         uint256 consummationSaleTimeframe; // at least 1 day
     }
-
     struct Conditions {
         uint256 floorPrice; // price defined by the owner, in USD
         PaymentTerms paymentTerms;
-        bytes32[] extras;
+    }
+    struct ExtraSaleTerms {
+        string label;
+        string customTerm;
     }
 
     /**
-     * @notice The seller (NFT asset owner) defines the conditions for the sale, which
-     *         includes but not limited to: minmum deposit amount to lock the asset, the
-     *         timeframe for the deposit, the timeframe for sale to be conclude, and any
+     * @notice The seller (NFT asset owner) defines the conditions for
+     *         the sale, which includes but not limited to: minmum
+     *         deposit amount to lock the asset, the timeframe for the
+     *         deposit, the timeframe for sale to be concluded, and any
      *         extra conditions that the seller requires.
      *
      * @param asset The adress of the asset NFT repository.
-     * @param assetId The id of the specific asset NFT.
      * @param conditions The conditions of the sale.
+     * @param extras Any extra terms required by the seller.
      */
     function setSaleConditions(
-        address asset,
-        uint256 assetId,
-        Conditions conditions
+        AssetNft asset,
+        Conditions memory conditions,
+        ExtraSaleTerms memory extras
     ) external;
 
     /**
-     * @notice The seller can update the conditions for the sale prior to the offer
-     *         approval.
-     *
-     * @param asset The adress of the asset NFT repository.
-     * @param assetId The id of the specific asset NFT.
-     * @param conditions The conditions of the sale.
+     * @notice The seller can update the conditions for the sale prior
+     *         to an deposit.
+     * @dev Same parameters as `setSaleConditions()`.
      */
     function updateSaleConditions(
         address asset,
-        uint256 assetId,
-        Conditions conditions
+        Conditions memory conditions,
+        ExtraSaleTerms memory extras
     ) external;
 }
