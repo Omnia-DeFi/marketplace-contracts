2db5799baed69ca2cf0ab138eb3f7a0bb3697b38
Théo RICHARD <theo.richard@omniadefi.com>
2022-08-22 12:09:17 +0100

    feat(interface): asset offer approval

--- src/interfaces/IAssetOfferApproval.sol
+++ src/interfaces/IAssetOfferApproval.sol
@@ -3,50 +3,65 @@ pragma solidity ^0.8.13;
 
 import {ISaleConditions} from "./ISaleConditions.sol";
 
-/**
- * @notice Contract that allows a seller to approve a buy request that was placed off-chain
- *         by a specific buyer.
- */
 interface IAssetOfferApproval {
-    struct FullSaleOfferApproval {
+    event OfferApprovedAtFloorPrice(
+        address indexed asset,
+        address indexed buyer,
+        ISaleConditions indexed conditions
+    );
+    event OfferApprovedAtCustomPrice(
+        address indexed asset,
+        address indexed buyer,
+        uint256 indexed salePrice,
+        ISaleConditions conditions
+    );
+
+    struct FloorPriceOfferApproval {
         address assetOwner;
         address buyer;
-        uint256 price;
-        bool isFloorPrice;
         uint256 approvalTimestamp;
         ISaleConditions conditions;
-        bool ownerSignature; // TODO: add owner signature
+        bool ownerSignature; // TODO: add owner's signature
+    }
+    struct CustomPriceOfferApproval {
+        address assetOwner;
+        address buyer;
+        bool atFloorPrice;
+        uint256 price;
+        uint256 approvalTimestamp;
+        ISaleConditions conditions;
+        bool ownerSignature; // TODO: add owner's signature
     }
 
     /**
-     * @notice Save a buy request for a full sale of a specific NFT asset at floor price.
+     * @notice Save a buy request for a specific NFT asset at floor
+     *         price.
      *
      * @param asset The adress of the asset NFT repository.
-     * @param assetId The id of the specific asset NFT.
      * @param buyer The buyer's address.
-     * @param salePrice Price agreed off-chain by both parties.
+     * @param conditions Conditions of the sale for this asset.
      */
     // TODO: pass owner's signature
-    function fullSaleApprovalAtFloorPriceOf(
+    function approveSaleOfAtFloorPrice(
         address asset,
-        uint256 assetId,
-        address buyer
+        address buyer,
+        ISaleConditions conditions
     ) external;
 
     /**
-     * @notice Save a buy request for a full sale of a specific NFT asset with a price
-     *         different from the floor price and agreed off-chain with the buyer.
+     * @notice Save a buy request for a specific NFT asset with a custom
+     *         price agreed off-chain with the buyer.
      *
      * @param asset The adress of the asset NFT repository.
-     * @param assetId The id of the specific asset NFT.
      * @param buyer The buyer's address.
      * @param salePrice Price agreed off-chain by both parties.
+     * @param conditions Conditions of the sale for this asset.
      */
     // TODO: pass owner's signature
-    function fullSaleApprovalWithCustomPriceOf(
+    function approveSaleOfAtCustomPrice(
         address asset,
-        uint256 assetId,
         address buyer,
-        uint256 salePrice
+        uint256 salePrice,
+        ISaleConditions conditions
     ) external;
 }
