b82ce8426c46ae6d6c2752b8a62dae1e02b45b4b
Théo RICHARD <theo.richard@omniadefi.com>
2022-08-24 13:30:43 +0100

    feat(modifier): define modifiers when setting sale conditions

--- src/SaleConditions.sol
+++ src/SaleConditions.sol
@@ -18,6 +18,63 @@ import {Marketplace} from "./Marketplace.sol";
  *      (before desposit), etc...
  */
 contract SaleConditions is ISaleConditions {
+    Marketplace public marketplace;
+
+    mapping(AssetNft => Conditions) public saleConditionsOf;
+    mapping(AssetNft => ExtraSaleTerms) public extraSaleConditionsOf;
+
+    constructor(Marketplace marketplace_) {
+        marketplace = marketplace_;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                 MODIFIERS
+    //////////////////////////////////////////////////////////////*/
+    modifier onlyAssetOwner(AssetNft assetNft) {
+        // require(assetNft.owner(assetId) == msg.sender, "NOT_OWNER");
+        _;
+    }
+
+    modifier minimalSaleConditions(
+        ISaleConditions.Conditions memory conditions
+    ) {
+        require(conditions.floorPrice > 0, "ZERO_FLOOR_PRICE");
+        require(
+            conditions.paymentTerms.consummationSaleTimeframe >= 24 hours,
+            "MIN_24H_SALE"
+        );
+        _;
+    }
+
+    modifier checkExtrasTerms(ExtraSaleTerms memory extras) {
+        if (bytes(extras.label).length > 0) {
+            require(bytes(extras.label).length >= 4, "4_CHAR_LABEL");
+            require(bytes(extras.customTerm).length >= 4, "4_CHAR_TERM");
+        }
+        _;
+    }
+
+    modifier conditionsNotSetYet(AssetNft assetNft) {
+        require(
+            saleConditionsOf[assetNft].floorPrice == 0,
+            "CONDITIONS_SET_PRICE"
+        );
+        require(
+            saleConditionsOf[assetNft].paymentTerms.consummationSaleTimeframe ==
+                0,
+            "CONDITIONS_SET_TIME"
+        );
+        require(
+            bytes(extraSaleConditionsOf[assetNft].label).length == 0,
+            "EXTRAS_SET"
+        );
+        require(
+            bytes(extraSaleConditionsOf[assetNft].customTerm).length == 0,
+            "EXTRAS_SET"
+        );
+        _;
+    }
+
     /// @inheritdoc ISaleConditions
     function setSaleConditions(
         AssetNft asset,
