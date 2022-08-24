c0592fe4a4166b60b6aa2aab3b4c4a301259aa5f
Théo RICHARD <theo.richard@omniadefi.com>
2022-08-24 19:40:05 +0100

    test: add mock files for Marketplace & AssetNft

---
+++ test/mock/MockMarketplace.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: SPDX-2.0-or-later
+pragma solidity ^0.8.13;
+
+import {Marketplace, AssetListing, SaleConditions} from "src/Marketplace.sol";
+
+contract MockMarketplace is Marketplace {
+    constructor() {}
+}
