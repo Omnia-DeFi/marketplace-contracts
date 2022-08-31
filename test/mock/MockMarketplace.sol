a40eee721a86062fdc82098665d3b245d4e2e296
Théo RICHARD <theo.richard@omniadefi.com>
2022-08-31 13:52:30 +0100

    test(mock): mock Marketplace data using functions

--- test/mock/MockMarketplace.sol
+++ test/mock/MockMarketplace.sol
@@ -1,10 +1,64 @@
 // SPDX-License-Identifier: SPDX-2.0-or-later
 pragma solidity ^0.8.13;
 
-import {Marketplace, AssetNft} from "src/Marketplace.sol";
+import {Marketplace, ListingLib, AssetNft, SaleConditions, OfferApproval} from "src/Marketplace.sol";
+import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
 
 contract MockMarketplace is Marketplace {
     function resetSale(AssetNft asset) public {
         _resetSale(asset);
+    }
+
+    function mockAssetListing(AssetNft asset)
+        public
+        returns (ListingLib.Status)
+    {
+        listingStatusOf[asset] = ListingLib.Status.ActiveListing;
+
+        return listingStatusOf[asset];
+    }
+
+    function mockSaleConditions(AssetNft asset)
+        public
+        returns (Conditions memory, ExtraSaleTerms memory)
+    {
+        Conditions memory conditions;
+        ExtraSaleTerms memory extras;
+
+        conditions.floorPrice = 1249014 * FIAT_PRICE_DECIMAL;
+        conditions.paymentTerms.consummationSaleTimeframe = 24 hours;
+
+        extras.label = "Label";
+        extras.customTermDescription = "Custom Description";
+
+        _setSaleConditions(asset, conditions, extras);
+
+        return (saleConditionsOf[asset], extraSaleConditionsOf[asset]);
+    }
+
+    function approveSaleOfAtFloorPrice(
+        AssetNft asset,
+        address buyer,
+        SaleConditions.Conditions memory conditions,
+        SaleConditions.ExtraSaleTerms memory extras
+    ) public returns (OfferApproval.Approval memory) {
+        _approveSaleOfAtFloorPrice(asset, buyer, conditions, extras);
+
+        return approvedOfferOf[asset];
+    }
+
+    function emitDepositAskAndBuyerDepositWithERC20Approved(
+        AssetNft asset,
+        address erc20,
+        string memory erc20Label,
+        OfferApproval.Approval memory approval
+    ) public {
+        _emitDepositAsk(asset, approval);
+        _buyerWholeDepositERC20(asset, erc20, erc20Label);
+    }
+
+    function sellerDepositAndSwap(AssetNft asset) public {
+        _sellerDepositAssetNft(asset);
+        _swapAssets(asset);
     }
 }
