b679e051b33048f19b8948229435b6f319f2890d
Théo RICHARD <theo.richard@omniadefi.com>
2022-08-31 11:45:47 +0100

    test(pre-config-data): `setUpWithMockedData`

--- test/mock/MockAssetListing.sol
+++ test/mock/MockAssetListing.sol
@@ -2,9 +2,23 @@
 pragma solidity ^0.8.13;
 
 import {AssetListing, AssetNft} from "../../src/AssetListing.sol";
+import "../../src/libraries/ListingLib.sol";
 
 contract MockAssetListing is AssetListing {
     function listAsset(AssetNft asset) public {
         _listAsset(asset);
+    }
+
+    function setUpWithMockedData(AssetNft asset)
+        public
+        returns (ListingLib.Status)
+    {
+        _setUpWithMockedData(asset);
+
+        return listingStatusOf[asset];
+    }
+
+    function _setUpWithMockedData(AssetNft asset) public {
+        listingStatusOf[asset] = ListingLib.Status.ActiveListing;
     }
 }
