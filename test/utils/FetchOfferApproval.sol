f395d910112923fa6670501fed6c4aa15ac859be
Théo RICHARD <theo.richard@omniadefi.com>
2022-09-05 21:20:20 +0100

    test: add create offer at floor price

--- test/utils/FetchOfferApproval.sol
+++ test/utils/FetchOfferApproval.sol
@@ -1,6 +1,22 @@
 import {OfferApproval, AssetNft, SaleConditions} from "../../src/OfferApproval.sol";
 
 library FetchOfferApproval {
+    function createFloorPriceApproval(
+        address seller,
+        address buyer,
+        SaleConditions.Conditions memory conditions,
+        SaleConditions.ExtraSaleTerms memory extras
+    ) public view returns (OfferApproval.Approval memory approval) {
+        approval.seller = seller;
+        approval.buyer = buyer;
+        approval.atFloorPrice = true;
+        approval.price = 650000 * 10**18;
+        approval.approvalTimestamp = block.timestamp;
+        approval.conditions = conditions;
+        approval.extras = extras;
+        approval.ownerSignature = true;
+    }
+
     function createCustomPriceApproval(
         address seller,
         address buyer,
